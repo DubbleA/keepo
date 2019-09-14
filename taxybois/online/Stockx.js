fb7e776008570ba08bb9e88cf4c33d15933c9d65
Ethan Reiland <ethancreiland@gmail.com>
2019-09-14 14:35:56 -0400

    starting to turn the stockx class into something that follows standards

--- taxybois/online/Stockx.js
+++ taxybois/online/Stockx.js
@@ -2,14 +2,8 @@ const rp = require('request-promise');
 var cookieJar = rp.jar();
 
 module.exports = class Stockx {
-  constructor(username, password) {
-    this.asyncConstructor(username, password);
-  }
-
-  async asyncConstructor(username, password) {
-    await this.authenticate(username, password);
-    await this.getSales();
-  }
+  //fucking does nothing lmao
+  constructor() {}
 
   authenticate(username, password) {
     return new Promise((resolve, reject) => {
@@ -47,44 +41,46 @@ module.exports = class Stockx {
     });
   }
 
-  getSales() {
-    return new Promise((resolve, reject) => {
-      let page = 1;
-      {
-        let options = {
-          url : `https://stockx.com/api/customers/${this.customerId}/selling/history`,
-          method: 'GET',
-          headers: {
-            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
-            'jwt-authorization': this.jwtToken
-          },
-          qs: {
-            sort: 'matched_with_date',
-            order: 'DESC',
-            limit: '100',
-            page: page,
-            currency: 'USD'
-          },
-          resolveWithFullResponse : true,
-          json: true,
-          jar: cookieJar
-        }
+  async getAllSales() {
+    let sales = [];
+    //
+  }
 
-        rp(options)
-          .then((response) => {
-            var resp = response.body.PortfolioItems[0];
-            console.log(resp)
-            // if (!response.body.Pagination.total) {
-            //   resolve();
-            // }
-            // page++;
-            resolve();
-          })
-          .catch((error) => {
-            console.log(error);
-            reject();
-          });
+  getSale(page) {
+    //Just a warning incase someone fucks up
+    if (!this.customerId) {
+      console.warn('Please wait for setup to finish before calling this function');
+      return;
+    }
+    return new Promise((resolve, reject) => {
+      let options = {
+        url : `https://stockx.com/api/customers/${this.customerId}/selling/history`,
+        method: 'GET',
+        headers: {
+          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+          'jwt-authorization': this.jwtToken
+        },
+        qs: {
+          sort: 'matched_with_date',
+          order: 'DESC',
+          limit: '100',
+          page: page,
+          currency: 'USD'
+        },
+        resolveWithFullResponse : true,
+        json: true,
+        jar: cookieJar
       }
+
+      rp(options)
+        .then((response) => {
+          console.log(response)
+          response.body.Pagination.total ? resolve(response.body.PortfolioItems) : reject();
+        })
+        .catch((error) => {
+          console.log(error);
+          reject();
+      });
     });
   }
 }
--- taxybois/online/test.js
+++ taxybois/online/test.js
@@ -1,2 +1,8 @@
 const Stockx = require('./Stockx.js');
-let oof = new Stockx('Aaryan.gupta158@gmail.com', 'NigerianPrince1!');
+let oof = new Stockx();
+oof.authenticate('Aaryan.gupta158@gmail.com', 'NigerianPrince1!')
+    .then(() => {
+        oof.getSale(1);
+    }).catch(error => {
+        console.log('lmao i fucked up big time');
+    });
