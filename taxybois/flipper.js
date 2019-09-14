83420fbb9db2990b5617d1bc70138f7133784bb6
DubbleA <dubblea111@gmail.com>
2019-09-13 23:37:43 -0400

    a

--- taxybois/flipper.js
+++ taxybois/flipper.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 function main (){
 
     var bobbert = csvConv('./brendan.csv');
-    console.log(bobbert);
-    storeString(bobbert);
+    //console.log(bobbert);
+    storeStringswift(bobbert);
 
 
 
@@ -25,6 +25,7 @@ function csvConv(csvFilePath) {
         }
         else {
             finaljson.push(jsonObj[i]);
+            //console.log(jsonObj[i])
         }
 
     }
@@ -32,31 +33,58 @@ function csvConv(csvFilePath) {
     return finaljson;
 }
 
+function ifCity(parsed, i){
+    let final = [];
+    var item = ''; 
+    var brand= '';
+    var SKU = '';
+    var retail = '';
+        if (parsed[i].Item != null || (parsed[i].brand.length != 0)) {
+            brand = parsed[i].brand;
+            final.push(brand);
+        }
+        if (parsed[i].Item != null || (parsed[i].Item.length != 0)) {
+            item = parsed[i].Item;
+            final.push(item);
+        }
+        if (parsed[i].SKU != null || (parsed[i].SKU.length != 0)) {
+            SKU = parsed[i].SKU;
+            final.push(SKU);
+        }
+        if (parsed[i].retail != null || (parsed[i].retail.length != 0)) {
+            retail = parsed[i].retail;
+            final.push(retail);
+        }
+        console.log(final);
+    return final;
+}
+
 function storeStringswift(parsed) {
-    let final = []
-    console.log(parsed[0].brand);
+    let final = [];
+    //console.log(parsed[0].brand);
 
     for (i = 0; i < parsed.length; i++) {
         //console.log(parsed[i].brand);
-
+        //ifCity(parsed, i);
         var inventory = {
             category: "",
             brand: parsed[i].brand,
-            name: parsed[i].name,
+            name: parsed[i].Item,
             SKU: parsed[i].SKU,
-            retail: "",
+            retail: parsed[i].Cost,
+            shippingCost: parsed[i].Shipping,
             color: "",
-            size: parsed[i].size,
+            size: parsed[i].Size,
             condition: "new",
             notes: ""
         }
         var sale = {
-            market: parsed[i].platform,
+            market: parsed[i].Platform,
             orderNum: parsed[i].orderNum,
             trackingNum: "",
-            saleDate: parsed[i].date,
-            soldPrice: parsed[i].sellingPrice,
-            totalAfterFees: parsed[i].totalAfterFees,
+            saleDate: parsed[i].Date,
+            soldPrice: parsed[i].SellingPrice,
+            totalAfterFees: parsed[i].TotalAfterFees,
             notes: ""
         }
         var expense = {
@@ -79,6 +107,8 @@ function storeStringswift(parsed) {
             jso
         }, null, 2).length - 1)));
     }
+
+    var tempdata = '[' + final.slice(0, parsed.length + 1) + ']';
 
     fs.writeFile('largebasicf.json', '[' + final.slice(0, parsed.length + 1) + ']', 'utf8');
 
