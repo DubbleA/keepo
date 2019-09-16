b26989ad4aa8d3870ea7dcc85a7be19092b7500b
Ethan Reiland <ethancreiland@gmail.com>
2019-09-16 14:30:47 -0400

    asduasuhdahfo

--- taxybois/online/Stockx.js
+++ taxybois/online/Stockx.js
@@ -42,17 +42,20 @@ module.exports = class Stockx {
   }
 
   async getAllSales() {
+    //Just a warning incase someone fucks up
+    if (!this.customerId) {
+      console.warn('Please wait for setup to finish before calling this function');
+      return;
+    }    
     let sales = [];
     let page = 1;
-    try {
-      while (true) {
-        console.log((await this.getSalePage(page)).length);
-        //sales = sales.concat(sales, await this.getSalePage(page));
-        page++;
-      }
-    } catch (error) {
-      return sales;
+    let max = 2;
+    while (page <= max) {
+      console.log((await this.getSalePage(page)).length);
+      //sales = sales.concat(sales, await this.getSalePage(page));
+      page++;
     }
+    return sales;
   }
 
   getSalePage(page) {
@@ -84,6 +87,37 @@ module.exports = class Stockx {
       rp(options)
         .then((response) => {
           response.body.Pagination.total ? resolve(response.body.PortfolioItems) : reject();
+        })
+        .catch((error) => {
+          console.log(error);
+          reject();
+      });
+    });
+  }
+
+  search(searchTerm) {
+    return new Promise((resolve, reject) => {
+      let options = {
+        url : `https://xw7sbct9v6-2.algolianet.com/1/indexes/products`,
+        method: 'GET',
+        // headers: {
+        //   'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36',
+        // },
+        qs: {
+          'x-algolia-agent': 'Algolia for vanilla JavaScript 3.32.1',
+          'x-algolia-application-id': 'XW7SBCT9V6',
+          'x-algolia-api-key': '6bfb5abee4dcd8cea8f0ca1ca085c2b3'
+        },
+        formData: {
+          params: `"query="${searchTerm}&facets=*&filters=""`
+        },
+        resolveWithFullResponse : true,
+        json: true,
+      }
+
+      rp(options)
+        .then((response) => {
+          resolve(response);
         })
         .catch((error) => {
           console.log(error);
--- taxybois/online/test.js
+++ taxybois/online/test.js
@@ -2,7 +2,7 @@ const Stockx = require('./Stockx.js');
 let oof = new Stockx();
 oof.authenticate('Aaryan.gupta158@gmail.com', 'NigerianPrince1!')
     .then(async () => {
-        console.log((await oof.getAllSales()).length);
+        console.log((await oof.getAllSales()));
     })
     .catch(error => {
         console.log('lmao i fucked up big time');
