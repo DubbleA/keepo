f5b90add1e1bd30b98f9534a9db12ebcfb88509e
Ethan Reiland <20ereiland@student.dasd.org>
2019-10-24 20:15:18 -0400

    bruh moment

--- useless bullshit/tables.js
+++ useless bullshit/tables.js
@@ -1,28 +1,8 @@
-function appendTable(...args) {
-    let table = document.getElementById('bruh');
-    let newRow = document.createElement('tr');
-    args.forEach(colData => {
-        // if (typeof colData == 'object') {
-        //     var className = colData.class;
-        //     var styles = colData.styles;
-        //     var id = colData.id;
-        //     colData = colData.innerHTML;
-        // }
-        // "JuSt CoMmEnT It OuT" - Retard
-        // "Push to github, do it just incase" - Retard
-        newRow.insertAdjacentHTML('beforeend', `<td id="${colData.id||''}" class="${colData.className||''}" style="${colData.styles||''}">${colData.innerHTML||colData}</td>`);
-    });
-    table.appendChild(newRow);
-    // table.insertAdjacentHTML('beforeend',
-    // `<tr id="${'aaa'}">
-    //     <td class="text-center">${table.childElementCount+1}<td>
-    //     <td>${name}</td>
-    //     <td>${site}</td>
-    //     <td class="text-center">${lastRefresh}</td>
-    //     <td class="text-right">${totalSales}</td>
-    //     <td class="text-right">
-    //         <button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title=""><i class="tim-icons icon-refresh-01"></i></button>
-    //         <button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title=""><i class="tim-icons icon-simple-remove"></i></button>
-    //     </td>
-    // </tr>`);
+function appendTable(tableId, ...args) {
+    let table = document.getElementById(tableId);
+    let row = document.createElement('tr');
+    args.forEach(colData => row.insertAdjacentHTML('beforeend', `<td id="${colData.id||''}" class="${colData.className||''}" style="${colData.styles||''}">${colData.code ? eval(colData.code) : colData.innerHTML||colData}</td>`));
+    table.appendChild(row);
 }
+
+appendTable('bruh', {className:'text-center',code:'table.childElementCount+1'}, 'Mike', 'StockX', {className:'text-center',innerHTML:2013}, {className:'text-right',innerHTML:'$1,100'}, {className:'text-right',innerHTML: '<button type="button" rel="tooltip" class="btn btn-success btn-link btn-sm btn-icon " data-original-title="Refresh" title=""><i class="tim-icons icon-refresh-01"></i></button><button type="button" rel="tooltip" class="btn btn-danger btn-link btn-sm " data-original-title="Delete" title=""><i class="tim-icons icon-simple-remove"></i></button>'});
